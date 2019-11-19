f0b0758d57d3f37c8e900a7a747e8ddaee9a59e5
AdamGruber <adam.gruber@cpnv.ch>
2019-11-19 19:41:33 +0100

    Exercice des dates partie 1
    Cette exercice consiste a afficher des dates de différentes manières en utilisant la fonction date()

---
+++ Dates.php
@@ -0,0 +1,9 @@
+<?php
+//Display dates
+echo date("l d F Y") . "<br><br>";
+echo date("M jS Y"). "<br><br>";
+echo date("j/m/y g:i a"). "<br><br>";
+echo date('j M Y, H:i:s'). '<br><br>';
+echo date('r')
+
+?>
