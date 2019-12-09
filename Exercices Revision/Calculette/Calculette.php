936ab4371a337c89457aed674022d3d9ed810e5f
GRUBER Adam <adam.gruber@cpnv.ch>
2019-12-09 09:08:10 +0100

    Calculette
    Exercice calculette terminé

--- Exercices Revision/Calculette/Calculette.php
+++ Exercices Revision/Calculette/Calculette.php
@@ -4,24 +4,30 @@ $op1 = 3;
 $op2 = 7;
 $resultOperation = 0;
 
-
+//Get the result of the add function
 $resultOperation = add($op1,$op2);
-echo $resultOperation . '<br>';
+echo 'Le calcul est '.$op1.' + '.$op2.' = '.$resultOperation.'<br>'; //echo text and var
+//Get the result of the sub function
 $resultOperation = sub($op1,$op2);
-echo $resultOperation . '<br>';
+echo 'Le calcul est '.$op1.' - '.$op2.' = '.$resultOperation.'<br>'; //echo text and var
+//Get the result of the mul function
 $resultOperation = mul($op1,$op2);
-echo $resultOperation . '<br>';
+echo 'Le calcul est '.$op1.' * '.$op2.' = '.$resultOperation.'<br>'; //echo text and var
 
+return 0;
+//Add both numbers and return the result
 function add($op1,$op2){
     $tempAdd = $op1 + $op2;
     return $tempAdd;
 }
 
+//Substract both numbers and return the result
 function sub($op1,$op2){
     $tempAdd = $op1 - $op2;
     return $tempAdd;
 }
 
+//Multiply both numbers and return the result
 function mul($op1,$op2){
     $tempAdd = $op1 * $op2;
     return $tempAdd;
